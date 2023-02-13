350cd4f06eae247a3633f3edca41d0163ec52952
Tripatjot Singh <tripatjot1455.be21@chitkara.edu.in>
2023-02-13 12:25:33 +0530

    dashboard added setting

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useLocation, useNavigate } from 'react-router-dom';
 import BarChartIcon from '@mui/icons-material/BarChart';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const links = [
     {
@@ -80,6 +81,15 @@ function Link({ item }) {
 function Dashboard({ component }) {
 
     let navigate = useNavigate();
+    const [setting, setSetting] = useState({
+        translate: "0px",
+        transform: "rotate(0deg)",
+    });
+    const [arrow, setArrow] = useState({
+        opacity: "0",
+        translate: "0px"
+    });
+    const [drawer, setDrawer] = useState("0%");
 
     return (
         <Box sx={{
@@ -91,22 +101,37 @@ function Dashboard({ component }) {
             backgroundSize: "100% 100%"
         }}>
             <Box sx={{
-                width: "20%",
+                width: "300px",
+                background: "#000C2A",
+                backgroundSize: "100% 100%",
                 display: "flex",
                 flexDirection: "column",
+                position: {md: "relative", lg: "relative", xl: "relative", sm: "fixed", xs: "fixed"},
+                left: "0%",
+                top: "0%",
+                height: {sm: "100%", xs: "100%", md: "100vh", lg: "100vh", xl: "100vh"}
             }}>
+                <img style={{
+                    position: "absolute",
+                    top: "0%",
+                    left: "0%",
+                    width: "100%",
+                    height: "100%",
+                    zIndex: "1"
+                }} src="http://www.gstatic.com/mobilesdk/190424_mobilesdk/nav_nachos@2x.png" alt="triangles" />
                 <Box sx={{
                     height: "25%",
+                    minHeight: "130px",
                     display: "flex",
                     flexDirection: "column",
-                    justifyContent: "flex-end"
+                    justifyContent: "flex-end",
+                    zIndex: "20"
                 }}>
                     <Box onClick={() => {
                         navigate("/dashboard");
                     }} sx={{
                         padding: "12px",
                         borderTop: "1px solid white",
-                        width: "100%",
                         display: "flex",
                         gap: "16px",
                         alignItems: "center",
@@ -114,21 +139,108 @@ function Dashboard({ component }) {
                             cursor: "pointer"
                         },
                     }}>
-                        <BarChartIcon sx={{
-                            color: "white"
-                        }} />
+                        <Box sx={{
+                            width: "24px",
+                            height: "24px",
+                            borderRadius: "50%",
+                            overflow: "hidden"
+                        }}>
+                            <img style={{
+                                width: "100%",
+                                height: "100%"
+                            }} src={require("../../Assets/logo.png")} alt='logo' />
+                        </Box>
                         <Typography sx={{
                             color: "white",
                             fontSize: "18px",
-                        }}>COMPANY OVERVIEW</Typography>
+                        }}>COMPANY</Typography>
+                    </Box>
+                    <Box sx={{
+                        borderTop: "1px solid white",
+                        width: "100%",
+                        display: "flex",
+                        alignItems: "center",
+                        // padding: "12px"
+                    }}>
+                        <Box onClick={() => {
+                            navigate("/dashboard");
+                        }} sx={{
+                            padding: "12px 8px",
+                            alignItems: "center",
+                            gap: "16px",
+                            display: "flex",
+                            "&:hover": {
+                                cursor: "pointer"
+                            }
+                        }}>
+                            <BarChartIcon sx={{
+                                color: "white",
+                                width: "30px",
+                                height: "30px",
+                            }} />
+                            <Typography sx={{
+                                color: "white",
+                                fontSize: "14px",
+                            }}>COMPANY OVERVIEW</Typography>
+                        </Box>
+                        <Box onMouseEnter={() => {
+                            setSetting({
+                                translate: "-6px",
+                                transform: "rotate(-45deg)",
+                            });
+                            setArrow({
+                                opacity: "1",
+                                translate: "12px"
+                            });
+                        }} onMouseLeave={() => {
+                            setSetting({
+                                translate: "0px",
+                                transform: "rotate(0deg)",
+                            });
+                            setArrow({
+                                opacity: "0",
+                                translate: "0px",
+                            });
+                        }} sx={{
+                            margin: "12px",
+                            "&:hover": {
+                                cursor: "pointer"
+                            },
+                            position: "relative",
+                            height: "45%",
+                            flexGrow: "1",
+                            borderLeft: "1px solid white",
+                            paddingLeft: "25px"
+                        }}>
+                            <Box sx={{
+                                transition: "0.1s",
+                                ...setting,
+                                position: "absolute"
+                            }}>
+                                <SettingsIcon sx={{
+                                    color: "white"
+                                }} />
+                            </Box>
+                            <Box sx={{
+                                transition: "0.1s",
+                                ...arrow,
+                                position: "absolute",
+                            }}>
+                                <ArrowRightIcon sx={{
+                                    color: "white"
+                                }} />
+                            </Box>
+                        </Box>
                     </Box>
                 </Box>
                 <Box sx={{
-                    height: "65%",
+                    height: "75%",
+                    minHeight: "200px",
                     borderTop: "1px solid white",
                     display: "flex",
                     padding: "15% 16px 0px 16px",
-                    justifyContent: "center"
+                    justifyContent: "center",
+                    zIndex: "20"
                 }}>
                     <Box sx={{
                         width: "90%",
@@ -146,30 +258,12 @@ function Dashboard({ component }) {
                         }
                     </Box>
                 </Box>
-                {/* <Box sx={{
-                        height: "10%",
-                        padding: "16px",
-                        borderTop: "1px solid white",
-                        display: "flex",
-                        justifyContent: "center"
-                    }}>
-                        <Box sx={{
-                            width: "70%"
-                        }}>
-                            <SettingsIcon fontSize='large' sx={{
-                                color: "white",
-                                "&:hover": {
-                                    cursor: "pointer"
-                                }
-                            }} />
-                        </Box>
-                    </Box> */}
             </Box>
             <Box sx={{
-                width: "80%",
+                flexGrow: "1",
                 background: "rgba(226, 234, 255, 1)",
                 // background: "rgba(0,0,0,0.2)",
-                margin: "16px 16px 16px 0px",
+                margin: {md: "16px 16px 16px 0px", lg: "16px 16px 16px 0px", xl: "16px 16px 16px 0px", sm: "16px", xs: "16px"},
                 borderRadius: "16px",
                 display: "flex",
                 flexDirection: "column"
