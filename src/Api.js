151978a006643226e31493f93f6a6025727f33f5
IshanSingla <is.ishan.singla@gmail.com>
2023-02-26 11:59:20 +0530

    Update: Api Updated with New Host

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const publicApi = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://crm.ishansingla.me",
 });
 // export const publicApi = axios.create({
 //   baseURL: "https://crm-backend-smoky.vercel.app",
