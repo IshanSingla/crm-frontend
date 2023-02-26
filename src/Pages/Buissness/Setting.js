0ceff94be7a2e4c5b52c9fe02484800fdb9e4cb9
ItzAnsh <anshbhasin3723@gmail.com>
2023-02-27 00:13:03 +0530

    Update: UI of settings page

--- src/Pages/Buissness/Setting.js
+++ src/Pages/Buissness/Setting.js
@@ -3,42 +3,66 @@ import { publicApi } from "../../Api";
 import { toast } from "react-toastify";
 
 export default function Settings({ currentUser, id }) {
-  const [data, setData] = useState();
+	const [data, setData] = useState();
 
-  useEffect(() => {
-    currentUser.getIdToken().then((token) => {
-      publicApi
-        .get(`/buissness/${id}`, {
-          headers: {
-            authorization: token,
-          },
-        })
-        .then((res) => {
-          setData(res.data.buissness);
-        })
-        .catch((err) => {
-          toast.error(err.message);
-        });
-    });
-  }, [currentUser, id]);
-  return data ? (
-    <div className="settings">
-      <div className="grid grid-cols-2">
-        <div className="name">Name</div>
-        <div className="value">{data.buissnessName}</div>
-        <div className="name">Email</div>
-        <div className="value">{data.buissnessContact.email}</div>
-        <div className="name">Phone Number</div>
-        <div className="value">+{data.buissnessContact.phone.code} {data.buissnessContact.phone.number}</div>
-        <div className="name">buissness Gst</div>
-        <div className="value">{data.buissnessGst}</div>
-        <div className="name">buissness Pan</div>
-        <div className="value">{data.buissnessPan}</div>
-        <div className="name">buissness Address</div>
-        <div className="value">{data.buissnessAddress??"None"}</div>
-      </div>
-    </div>
-  ) : (
-    <div className="h-fit justify-center items-center flex ">Loader</div>
-  );
+	useEffect(() => {
+		currentUser.getIdToken().then((token) => {
+			publicApi
+				.get(`/buissness/${id}`, {
+					headers: {
+						authorization: token,
+					},
+				})
+				.then((res) => {
+					setData(res.data.buissness);
+				})
+				.catch((err) => {
+					toast.error(err.message);
+				});
+		});
+	}, [currentUser, id]);
+	return data ? (
+		<div className="settings">
+			<div className="w-[100%] flex justify-center">
+				<div className="w-fit bg-white shadow-xl rounded-2xl">
+					<div className="p-5 h-full">
+						<div className="grid-cols-2 grid gap-10">
+							<div className="name font-bold">Name</div>
+							<div className="value">{data.buissnessName}</div>
+						</div>
+
+						<div className="grid grid-cols-2 gap-10">
+							<div className="name font-bold">Email</div>
+							<div className="value">{data.buissnessContact.email}</div>
+						</div>
+
+						<div className="grid grid-cols-2 gap-10">
+							<div className="name font-bold">Phone Number</div>
+							<div className="value">
+								+{data.buissnessContact.phone.code}{" "}
+								{data.buissnessContact.phone.number}
+							</div>
+						</div>
+
+						<div className="grid grid-cols-2 gap-10">
+							<div className="name font-bold">buissness Gst</div>
+							<div className="value">{data.buissnessGst}</div>
+						</div>
+
+						<div className="grid grid-cols-2 gap-10">
+							<div className="name font-bold">buissness Pan</div>
+							<div className="value">{data.buissnessPan}</div>
+						</div>
+
+						<div className="grid grid-cols-2 gap-10">
+							<div className="name font-bold">buissness Address</div>
+							<div className="value">{data.buissnessAddress ?? "None"}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	) : (
+		<div className="h-fit justify-center items-center flex ">Loader</div>
+	);
 }
