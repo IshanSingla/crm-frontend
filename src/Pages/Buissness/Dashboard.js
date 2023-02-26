dcedd70fc8b086c4a4c2243fca092e0e2611da49
ItzAnsh <anshbhasin3723@gmail.com>
2023-02-26 14:08:57 +0530

    Update: Added chart js | Bar chart made for dashboard

--- src/Pages/Buissness/Dashboard.js
+++ src/Pages/Buissness/Dashboard.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import BarChart from "./components/barChart";
+import { UserData } from "./components/data";
 
-export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+export default function Dashboard(chartdata) {
+	const [userData, setUserData] = useState({
+		labels: UserData.map((data) => data.year),
+		datasets: [
+			{
+				label: "Users Gained",
+				data: UserData.map((data) => data.userGain),
+			},
+		],
+	});
+	return (
+		<div className="grid grid-cols-12">
+			<div className="col-span-6">
+				<BarChart chartData={userData} />
+			</div>
+		</div>
+	);
 }
---
+++ src/Pages/Buissness/components/lineChart.js
@@ -0,0 +1,2 @@
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "react-chartjs";
